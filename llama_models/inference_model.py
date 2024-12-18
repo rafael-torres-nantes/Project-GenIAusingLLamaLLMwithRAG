1db895f3d3c5198355af3249544400f000f9005d
rafael-torres-nantes <rafatorresnantes@gmail.com>
2024-12-18 16:47:39 -0400

    refactor: adding comments

--- llama_models/inference_model.py
+++ llama_models/inference_model.py
@@ -12,18 +12,40 @@ LLAMA_MODEL_INFERENCE = os.getenv('LLAMA_MODEL_INFERENCE')
 LLAMA_URL = os.getenv('LLAMA_URL')
 
 class LLAMAInferenceModel:
+    """
+    Classe para gerenciar a inferência de modelos
+    LLAMA, incluindo o modelo de chat e o modelo de LLM.
+    """
     def __init__(self):
+        """
+        Construtor da classe LLAMAInferenceModel.
+        """
         self.LLAMA_MODEL = LLAMA_MODEL_INFERENCE
         self.LLAMA_URL = LLAMA_URL
         
     def invoke_chat_model(self, prompt):
-
+        """
+        Faz uma chamada ao modelo de chat LLAMA.
+        
+        Args:
+            prompt (str): A entrada para o modelo de chat.
+        Returns:
+            response (str): A resposta do modelo de chat.
+        """
         model = ChatOllama(model=self.LLAMA_MODEL, base_url=self.LLAMA_URL)
         response = model.invoke(prompt)
 
         return response
     
     def invoke_llm_model(self, prompt):
+        """
+        Faz uma chamada ao modelo de LLM LLAMA.
+        
+        Args:
+            prompt (str): A entrada para o modelo de chat.
+        Returns:
+            response (str): A resposta do modelo de chat.
+        """
         llm = OllamaLLM(model=self.LLAMA_MODEL)
         response = llm.invoke(prompt)
         
