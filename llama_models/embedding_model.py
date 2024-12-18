c3b468ec7cc4e6aaae1b735fe8cf306ef718e49b
rafael-torres-nantes <rafatorresnantes@gmail.com>
2024-12-18 16:47:30 -0400

    feat: implement generate_embedding

--- llama_models/embedding_model.py
+++ llama_models/embedding_model.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from langchain_ollama import OllamaEmbeddings 
+from langchain_ollama import OllamaEmbeddings
+import ollama
 
 load_dotenv()
 
@@ -15,9 +16,29 @@ class LLAMAEmbeddingModel:
     def get_embedding_model(self):
         """
         Obtém o modelo de embeddings LLAMA.
+
+        Returns:
+            embedding_model: Modelo de embeddings LLAMA
         """
         
         embedding_model = OllamaEmbeddings(model=self.LLAMA_MODEL)
 
         return embedding_model
 
+    def generate_embedding(self, text):
+        """
+        Gera embedding para um texto fornecido. 
+        
+        Args:
+            text (str): Texto para o qual o embedding será gerado.
+        Returns:
+            embedding: Embedding gerado para o texto.
+        """
+        response = ollama.embeddings(model=LLAMA_MODEL_EMBEDDING, prompt=text)
+        embedding = response["embedding"]
+        return embedding
+    
+if __name__ == "__main__":
+    llama_embedding_model = LLAMAEmbeddingModel()
+    embedding = llama_embedding_model.generate_embedding("Hello, world!")
+    print(embedding)
